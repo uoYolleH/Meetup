7d89e72c0bf41ca90ddd48f836585bf5d80c9eee
ItsMateo <me@itsmateo.dev>
2026-03-13 01:12:55 -0400

    fix: revert

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react(), vike()],
-  base:
-    command === "build"
-      ? (process.env.BASE_PATH ?? "/Meetup/")
-      : "/",
-}));
+});
