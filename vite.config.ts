4afc0af33d8d6027b40a03c04489288ca1772813
ItsMateo <me@itsmateo.dev>
2026-03-13 01:00:07 -0400

    fix: workflow???

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), vike()],
-});
+  base:
+    command === "build"
+      ? (process.env.BASE_PATH ?? "/Meetup/")
+      : "/",
+}));
